053de68c68b9e8fb1c6ff64cb1e3cf58f5e68523
fsheedy <fsheedy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-28 23:00:32 +0000

    correction of the version number
    git-svn-id: https://plugins.svn.wordpress.org/etsy-shop/trunk@618430 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- etsy-shop.php
+++ etsy-shop.php
@@ -7,7 +7,7 @@ Plugin Name: Etsy Shop
 Plugin URI: http://wordpress.org/extend/plugins/etsy-shop/
 Description: Inserts Etsy products in page or post using bracket/shortcode method.
 Author: Frédéric Sheedy
-Version: 0.8.1
+Version: 0.9.0
 */
 
 /*  
